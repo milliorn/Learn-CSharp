dabd9135b15d8c4f735fc6723912d30751f2561b
milliorn <scottmilliorn@gmail.com>
2020-04-22 16:35:58 -0700

    Walkthrough: Creating and Using Dynamic Objects (C# and Visual Basic)

---
+++ DynamicSample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicSample
+{
+    class Program
+    {
+        static void Main()
+        {
+            dynamic rFile = new ReadOnlyFile(@"..\..\TextFile1.txt");
+            
+            foreach(string line in rFile.Customer)
+            {
+                Console.WriteLine(line);
+            }
+            
+            Console.WriteLine("----------------------------");
+
+            foreach(string line in rFile.Customer(StringSearchOption.Contains, true))
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Free courses, tutorials, videos, and more about learning the programming languag
 <https://dotnet.microsoft.com/learn/csharp>\
 <https://docs.microsoft.com/en-us/dotnet/csharp/getting-started/>\
 <https://docs.microsoft.com/en-us/dotnet/csharp/walkthroughs>\
+<https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/types/walkthrough-creating-and-using-dynamic-objects>
