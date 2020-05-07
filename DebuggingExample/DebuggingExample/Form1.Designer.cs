33b7b2c9582fb5ebda1793954048811eab0d118c
milliorn <scottmilliorn@gmail.com>
2020-05-07 16:21:03 -0700

    init

---
+++ DebuggingExample/DebuggingExample/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace DebuggingExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.debugControl1 = new DebugControlLibrary.DebugControl();
+            this.SuspendLayout();
+            // 
+            // debugControl1
+            // 
+            this.debugControl1.DemoString = null;
+            this.debugControl1.Location = new System.Drawing.Point(316, 138);
+            this.debugControl1.Name = "debugControl1";
+            this.debugControl1.Size = new System.Drawing.Size(150, 150);
+            this.debugControl1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.debugControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DebugControlLibrary.DebugControl debugControl1;
+    }
+}
+
---
+++ DebuggingExample/README.md
@@ -0,0 +1,7 @@
+# Walkthrough: Debug Custom Windows Forms Controls at Design Time
+
+When you create a custom control, you will often find it necessary to debug its design-time behavior. This is especially true if you are authoring a custom designer for your custom control. For details, see Walkthrough: Creating a Windows Forms Control That Takes Advantage of Visual Studio Design-Time Features.
+
+You can debug your custom controls using Visual Studio, just as you would debug any other .NET Framework classes. The difference is that you will debug a separate instance of Visual Studio that is running your custom control's code.
+
+<https://docs.microsoft.com/en-us/dotnet/framework/winforms/controls/walkthrough-debugging-custom-windows-forms-controls-at-design-time>
