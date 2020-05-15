3451bcd9c1af639403426e04067e1b2863f5aefc
milliorn <scottmilliorn@gmail.com>
2020-05-15 14:56:53 -0700

    HelloWorld

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hellow World!");
+            Console.WriteLine("What is your name? ");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey(true);
+        }
+
+    }
+}
---
+++ HelloWorld/README.md
@@ -0,0 +1,5 @@
+# Tutorial: Create your first .NET Core console application in Visual Studio 2019
+
+This article provides a step-by-step introduction to create and run a Hello World .NET Core console application in Visual Studio 2019. A Hello World application is traditionally used to introduce beginners to a new programming language. This program simply displays the phrase "Hello World!" on the screen.
+
+<https://docs.microsoft.com/en-us/dotnet/core/tutorials/with-visual-studio?tabs=csharp>
