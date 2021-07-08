3c63b213ba1f7fd8d9b83ab598bd05a353431ae7
Scott Milliorn <scottmilliorn@gmail.com>
2021-07-07 19:38:03 -0700

    create json file

--- JSONHowTo/Program.cs
+++ JSONHowTo/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.IO;
+using System;
+using System.Text.Json;
 
 namespace JSONHowTo
 {
@@ -6,7 +8,21 @@ namespace JSONHowTo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("How to serialize and deserialize (marshal and unmarshal) JSON in .NET");
+            WeatherForecast weather = new WeatherForecast
+            {
+                Date = DateTime.Parse("2019-08-01"),
+                TemperatureCelsius = 25,
+                Summary = "Hot"
+            };
+
+            // The following example creates JSON as a string:
+            string jsonString = JsonSerializer.Serialize(weather);
+            Console.WriteLine(jsonString);
+
+            //The following example uses synchronous code to create a JSON file
+            string fileName = "WeatherForecast.json";
+            File.WriteAllText(fileName, jsonString);
         }
     }
 }
