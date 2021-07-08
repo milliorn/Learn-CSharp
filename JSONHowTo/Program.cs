339f7a67335e38d9cbb6d1484547a12547adcb7b
Scott Milliorn <scottmilliorn@gmail.com>
2021-07-07 20:10:30 -0700

    Deserialize a JSON string

--- JSONHowTo/Program.cs
+++ JSONHowTo/Program.cs
@@ -58,6 +58,38 @@ namespace JSONHowTo
             //  https://stackoverflow.com/questions/10940883/c-converting-byte-array-to-string-and-printing-out-to-console
             byte[] jsonUtf8Bytes = JsonSerializer.SerializeToUtf8Bytes(weather);
             Console.WriteLine(Encoding.UTF8.GetString(jsonUtf8Bytes));
+
+            jsonString =
+@"{
+  ""Date"": ""2019-08-01T00:00:00-07:00"",
+  ""TemperatureCelsius"": 25,
+  ""Summary"": ""Hot"",
+  ""DatesAvailable"": [
+    ""2019-08-01T00:00:00-07:00"",
+    ""2019-08-02T00:00:00-07:00""
+  ],
+  ""TemperatureRanges"": {
+                ""Cold"": {
+                    ""High"": 20,
+      ""Low"": -10
+                },
+    ""Hot"": {
+                    ""High"": 60,
+      ""Low"": 20
+    }
+            },
+  ""SummaryWords"": [
+    ""Cool"",
+    ""Windy"",
+    ""Humid""
+  ]
+}
+";
+            //  Deserialize a JSON string:
+            WeatherForecast weatherForecast = JsonSerializer.Deserialize<WeatherForecast>(jsonString);
+            Console.WriteLine($"Date: {weatherForecast.Date}");
+            Console.WriteLine($"TemperatureCelsius: {weatherForecast.TemperatureCelsius}");
+            Console.WriteLine($"Summary: {weatherForecast.Summary}");
         }
     }
 }
