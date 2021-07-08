de067b360c792c73df8e5a4cdd3985f0d409f775
Scott Milliorn <scottmilliorn@gmail.com>
2021-07-07 19:49:57 -0700

    added properties

--- JSONHowTo/WeatherForecast.cs
+++ JSONHowTo/WeatherForecast.cs
@@ -1,6 +1,9 @@
+using System.Collections;
 using System;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Collections.Generic;
+
 namespace JSONHowTo
 {
     public class WeatherForecast
@@ -8,5 +11,15 @@ namespace JSONHowTo
         public DateTimeOffset Date { get; set; }
         public int TemperatureCelsius { get; set; }
         public string Summary { get; set; }
+        public string SummaryField;
+        public IList<DateTimeOffset> DatesAvailable { get; set; }
+        public Dictionary<string, HighLowTemps> TemperatureRanges { get; set; }
+        public string[] SummaryWords { get; set; }
+    }
+
+    public class HighLowTemps
+    {
+        public int High { get; set; }
+        public int Low { get; set; }
     }
 }
