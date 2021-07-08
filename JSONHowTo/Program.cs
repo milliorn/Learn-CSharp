ebcca003d676952a36df6abaacdd2115e05c4cd7
Scott Milliorn <scottmilliorn@gmail.com>
2021-07-07 19:42:29 -0700

    create json async

--- JSONHowTo/Program.cs
+++ JSONHowTo/Program.cs
@@ -1,12 +1,13 @@
-﻿using System.IO;
-using System;
+﻿using System;
+using System.IO;
 using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace JSONHowTo
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("How to serialize and deserialize (marshal and unmarshal) JSON in .NET");
             WeatherForecast weather = new WeatherForecast
@@ -23,6 +24,13 @@ namespace JSONHowTo
             //The following example uses synchronous code to create a JSON file
             string fileName = "WeatherForecast.json";
             File.WriteAllText(fileName, jsonString);
+
+            // The following example uses asynchronous code to create a JSON file:
+            fileName = "WeatherForecastAsync.json";
+            using FileStream createStream = File.Create(fileName);
+            await JsonSerializer.SerializeAsync(createStream, weather);
+            await createStream.DisposeAsync();
+            Console.WriteLine(File.ReadAllText(fileName));
         }
     }
 }
